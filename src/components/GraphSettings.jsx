a756a0421d5b588aeeca0012632522a88a9c5762
Pierre Garnier <pierre.garnier1618@gmail.com>
2020-07-16 09:12:15 +0200

    minor changes

--- src/components/GraphSettings.jsx
+++ src/components/GraphSettings.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Menu, Form, Input, Switch } from 'antd';
+import { Menu, Form, Input, Switch, Row, Col } from 'antd';
 
 const { SubMenu } = Menu;
 
@@ -8,7 +8,7 @@ class GraphSettings extends React.Component{
 	constructor(props, context) {
 		super(props, context);
 		this.state = {
-			openKeys: ['sub1']
+			openKeys: []
             
 		};
 		this.rootSubmenuKeys = ['sub1', 'sub2', 'sub4'];
@@ -41,14 +41,18 @@ class GraphSettings extends React.Component{
 					onFinish={() => this.onFinish()}
 					onValuesChange={(value, allvalues) => this.updateValues(allvalues)}
 					initialValues={{
-						autoscale:false
+						name: this.props.name,
+						autoX:false
 					}}
 				>
 					<Form.Item
 						label="Name"
 						name="name"
 					>
-						<Input/>
+						<Input
+							style={{ width: 200}}
+							placeholder={this.props.name}
+						/>
 					</Form.Item>
 					<Menu
 						mode="inline"
@@ -57,32 +61,23 @@ class GraphSettings extends React.Component{
 						selectable={false}
 						theme="dark"
 					>
-						<SubMenu key="sub1" title="Data 1">
+						<SubMenu key="sub1" title="X Axis">
 							<Menu.Item key="1">
 								<Form.Item
-                                	label="Autoscale"
-									name="autoscale"
+									label="Autoscale"
+									name="autoX"
 								>
 									<Switch size="small"/>
 								</Form.Item>
 							</Menu.Item>
-							<Menu.Item key="2">Option 2</Menu.Item>
-							<Menu.Item key="3">Option 3</Menu.Item>
-							<Menu.Item key="4">Option 4</Menu.Item>
-						</SubMenu>
-						<SubMenu key="sub2" title="Navigation Two" disabled>
-							<Menu.Item key="5">Option 5</Menu.Item>
-							<Menu.Item key="6">Option 6</Menu.Item>
-							<SubMenu key="sub3" title="Submenu">
-								<Menu.Item key="7">Option 7</Menu.Item>
-								<Menu.Item key="8">Option 8</Menu.Item>
+							<SubMenu key="sub3" title="Data sets">
+								<Menu.Item key="2">Option 7</Menu.Item>
+								<Menu.Item key="3">Option 8</Menu.Item>
 							</SubMenu>
 						</SubMenu>
-						<SubMenu key="sub4" title="Navigation Three">
-							<Menu.Item key="9">Option 9</Menu.Item>
-							<Menu.Item key="10">Option 10</Menu.Item>
-							<Menu.Item key="11">Option 11</Menu.Item>
-							<Menu.Item key="12">Option 12</Menu.Item>
+						<SubMenu key="sub2" title="Y Axis">
+							<Menu.Item key="5">Option 5</Menu.Item>
+							<Menu.Item key="6">Option 6</Menu.Item>
 						</SubMenu>
 					</Menu>
 				</Form>
--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -101,35 +101,9 @@ class LineChart extends React.Component {
 					onOk={() => this.setSettingsVisible(false)}
 					onCancel={() => this.setSettingsVisible(false)}
 				>
-					{/* <Input placeholder="Rename card" onPressEnter={(e) => this.changeTitle(e.target.value)} />
-					<Menu
-						mode="inline"
-						openKeys={this.state.openKeys}
-						onOpenChange={(openKeys) => this.onOpenChange(openKeys)}
-					>
-						<SubMenu key="sub1" title="Data 1">
-							<Menu.Item key="1">Option 1</Menu.Item>
-							<Menu.Item key="2">Option 2</Menu.Item>
-							<Menu.Item key="3">Option 3</Menu.Item>
-							<Menu.Item key="4">Option 4</Menu.Item>
-						</SubMenu>
-						<SubMenu key="sub2" title="Navigation Two">
-							<Menu.Item key="5">Option 5</Menu.Item>
-							<Menu.Item key="6">Option 6</Menu.Item>
-							<SubMenu key="sub3" title="Submenu">
-								<Menu.Item key="7">Option 7</Menu.Item>
-								<Menu.Item key="8">Option 8</Menu.Item>
-							</SubMenu>
-						</SubMenu>
-						<SubMenu key="sub4" title="Navigation Three">
-							<Menu.Item key="9">Option 9</Menu.Item>
-							<Menu.Item key="10">Option 10</Menu.Item>
-							<Menu.Item key="11">Option 11</Menu.Item>
-							<Menu.Item key="12">Option 12</Menu.Item>
-						</SubMenu>
-					</Menu> */}
 					<GraphSettings
 						values={(values) => this.updateValues(values)}
+						name={this.state.title}
 					/>
 				</Modal>
 			</div>
